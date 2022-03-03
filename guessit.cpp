ebba32db7dfe41cbff10b588a79c2dcb341957b9
21020797 <100185869+dkt1205@users.noreply.github.com>
2022-03-04 00:23:19 +0700

    Update guessit.cpp

--- guessit.cpp
+++ guessit.cpp
@@ -49,8 +49,8 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-    if(number<randomNumber){answer="Your number is lower";}
-    if(number>randomNumber){answer="Your number is higher";}
+    if(number<randomNumber){answer="Your number is lower.";}
+    if(number>randomNumber){answer="Your number is higher.";}
     if(number==randomNumber){answer="Congratulation! You win.";}
 
 
@@ -82,7 +82,7 @@ bool checkContinuePlaying(char isContinued) {
     bool result = false;
     if(isContinued=='y')
     {
-        return true;
+        return result=true;
     }
     return result;
 }
