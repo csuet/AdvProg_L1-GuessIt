17478583b4ba7380e9fe532daeb585170c0d5c3a
21020797 <100185869+dkt1205@users.noreply.github.com>
2022-03-04 00:27:20 +0700

    Update guessit.cpp

--- guessit.cpp
+++ guessit.cpp
@@ -80,7 +80,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if(isContinued=='y')
+    if(isContinued=='y' or isContinued=="Y")
     {
         return result=true;
     }
