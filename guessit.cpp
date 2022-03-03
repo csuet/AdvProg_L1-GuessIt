98029d717e8951c5e35a8674ab1e6ec32ddc193e
Hiệu Vũ Trọng <test@example.com>
2022-03-03 23:04:03 +0700

    lan7

--- guessit.cpp
+++ guessit.cpp
@@ -96,7 +96,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying( char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if(isContinued == '0')
+    if(isContinued == '1')
     {
         result = true;
     }
