86323112aa6f5d73c9b80a8c78d410c3e164ee3f
quang1409thanh <97092690+quang1409thanh@users.noreply.github.com>
2022-03-05 00:00:13 +0700

    no fuction checkContinuePlaying and no getPlayerOpinion

--- guessit.cpp
+++ guessit.cpp
@@ -14,7 +14,7 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    return rand() % 100 ;
 }
 
 
@@ -26,8 +26,9 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int number1;
+    cin>>number1;
+    return number1;
 }
 
 
@@ -45,7 +46,17 @@ string getAnswer(int number, int randomNumber) {
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
-    string answer;
+   string answer;
+   if(number > randomNumber){ 
+         answer= "Your number is higher.";
+    }
+    else if(number < randomNumber){
+         answer= "Your number is lower.";
+    }
+    else{
+         answer= "Congratulation! You win.";
+   }
+    
 
     return answer;
 }
@@ -59,8 +70,12 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if(answer=="Congratulation! You win."){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 
