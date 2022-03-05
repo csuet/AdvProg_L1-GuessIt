1320e86bd087d2abe6aebf3a7b9a45d432cfdc32
Thang-Tien <cao.hl.thang@gmail.com>
2022-03-05 17:24:23 +0700

    6th test

--- guessit.cpp
+++ guessit.cpp
@@ -91,7 +91,7 @@ bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
 
     bool result = false;
-    if(isContinued = 'y')
+    if(isContinued = 'y' || isContunued == 'Y')
     {
         return true;
     }
