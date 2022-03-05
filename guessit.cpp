218634384ac6a277d16d0a1617bb4010b0f09600
PhamHoang16 <21020324@vnu.edu.vn>
2022-03-05 17:36:02 +0700

    commit

--- guessit.cpp
+++ guessit.cpp
@@ -13,8 +13,9 @@ using namespace std;
         number (int) : random number in range 1-100
 ***/
 int generateRandomNumber() {
-    // TODO: Return the random number in range 1 to 100
-    return 100;
+    srand(time(NULL));
+    int n = rand() % 100 + 1;
+    return n;
 }
 
 
@@ -26,8 +27,9 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int guessedNumber;
+    cin >> guessedNumber;
+    return guessedNumber;
 }
 
 
@@ -46,7 +48,15 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+    if(number == randomNumber){
+        answer = "Congratulation! You win.";
+    }
+    else if(number > randomNumber){
+        answer = "Your number is higher.";
+    }
+    else if(number < randomNumber){
+        answer = "Your number is lower.";
+    }
     return answer;
 }
 
@@ -59,8 +69,9 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    bool result = false;
+    if(answer == "Congratulation! You win.") result = true;
+    return result;
 }
 
 
@@ -72,8 +83,13 @@ bool checkSuccess(string answer) {
 ***/
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
-    bool result = false;
-
+    bool result;
+    if(isContinued == 'y' || isContinued == 'Y'){
+        result = true;
+    }
+    else if(isContinued = 'N' || isContinued == 'n'){
+        result = false;
+    }
     return result;
 }
 
@@ -87,7 +103,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cin >> isContinued;
     return isContinued;
 }
 
