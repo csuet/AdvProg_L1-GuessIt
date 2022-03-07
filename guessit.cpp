cc371cb553ff9bf0ab19495b42df1076e718c276
Minh <ngocminh.uet@gmail.com>
2022-03-07 17:49:22 +0700

    21020357 NguyenDoanNgocMinh

--- guessit.cpp
+++ guessit.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <bits/stdc++.h>
 #include "guessit.h"
 
 using namespace std;
@@ -14,7 +12,9 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    srand(time(0));
+    int x = rand()%100 + 1;
+    return x;
 }
 
 
@@ -26,8 +26,7 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int x; cin >> x; return x;
 }
 
 
@@ -45,9 +44,9 @@ string getAnswer(int number, int randomNumber) {
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
-    string answer;
-
-    return answer;
+    if(number == randomNumber)return "Congratulation! You win.";
+    if(number < randomNumber)return "Your number is lower.";
+    return "Your number is higher.";
 }
 
 
@@ -59,8 +58,8 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if(answer == "Congratulation! You win.")return 1;
+    return 0;
 }
 
 
@@ -72,7 +71,7 @@ bool checkSuccess(string answer) {
 ***/
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
-    bool result = false;
+    bool result = isContinued == 'Y' || isContinued == 'y' ? true : false;
 
     return result;
 }
@@ -87,7 +86,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cin >> isContinued;
     return isContinued;
 }
 
