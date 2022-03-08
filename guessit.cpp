c93e986d9231666e4c0941353a64965b7023bf59
21020084 <21020084@vnu.edu.vn>
2022-03-08 17:27:29 +0700

    submit

--- guessit.cpp
+++ guessit.cpp
@@ -14,7 +14,8 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    srand(time(NULL));
+    return rand() % 100 + 1;
 }
 
 
@@ -26,8 +27,8 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int number; cin >> number;
+    return number;
 }
 
 
@@ -47,6 +48,11 @@ string getAnswer(int number, int randomNumber) {
     ***/
     string answer;
 
+    if (number < randomNumber) answer = "Your number is lower.";
+    else {
+      if (number > randomNumber) answer = "Your number is higher.";
+      else answer = "Congratulation! You win.";
+    }
     return answer;
 }
 
@@ -59,7 +65,7 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
+    if (answer.substr(0, 3) == "You") return false;
     return true;
 }
 
@@ -74,6 +80,8 @@ bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
 
+    result = (isContinued == 'y' ? true : false);
+
     return result;
 }
 
@@ -87,6 +95,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
+
+    cin >> isContinued;
 
     return isContinued;
 }
