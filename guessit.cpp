435e26a9851449cb8fae0f6cbc30941ed33a3948
Kaito <21020277@vnu.edu.vn>
2022-03-08 10:30:57 +0700

    Complete guessit.cpp again

--- guessit.cpp
+++ guessit.cpp
@@ -77,7 +77,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if (isContinued == 'y') result = true;
+    if (isContinued == 'y' || isContinued == 'Y') result = true;
     return result;
 }
 
@@ -91,7 +91,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-    cout << "Do you want to continue playing? Enter 'y' or 'n': ";
+    cout << "Do you want to continue playing?";
     cin >> isContinued;
     return isContinued;
 }
