35743f7ae0e2ade32ba1868c64cd69c966c7fbd6
unknown <21020129@vnu.edu.vn>
2022-03-09 09:33:33 +0700

    BAT

--- guessit.cpp
+++ guessit.cpp
@@ -48,8 +48,8 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-    if(number > randomNumber) answer = "Your number is higher";
-    else if (number < randomNumber) answer = "Your number is lower";
+    if(number > randomNumber) answer = "Your number is higher.";
+    else if (number < randomNumber) answer = "Your number is lower.";
     else answer = "Congratulation! You win.";
     return answer;
 }
@@ -94,6 +94,7 @@ char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
     do{
+        playGuessIt();
         cout << endl <<"Do you want to play again (y/n) ?";
         cin >> isContinued;
     }while (isContinued == 'y' || isContinued == 'Y');
