2acf4e9012d769c9163b1c2986facd4b001b1966
hoanganhnth <you@example.com>
2022-03-10 18:43:26 +0700

    lan1

--- guessit.cpp
+++ guessit.cpp
@@ -14,7 +14,8 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+
+    return 1+rand()%100;
 }
 
 
@@ -25,9 +26,12 @@ int generateRandomNumber() {
         number (int) : the number that player guessed
 ***/
 int getPlayerGuess() {
+    int guess ;
+    cin>>guess;
+
     // TODO: Ask the player guest and return the player's number
 
-    return 1;
+    return guess;
 }
 
 
@@ -39,13 +43,16 @@ int getPlayerGuess() {
         answer (string) : answer of computer after checking result
 ***/
 string getAnswer(int number, int randomNumber) {
-    /*** 
+    /*
         TODO: check number with randomNumber and return the result.
               If number is higher than randomNumber, the answer is "Your number is higher."
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
-    ***/
+    */
     string answer;
+    if(number==randomNumber) answer="Congratulation! You win.";
+    else if(number>randomNumber) answer="Your number is higher.";
+    else answer="Your number is lower.";
 
     return answer;
 }
@@ -59,8 +66,9 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
+    if(answer=="Congratulation! You win.") return true;
+    else return false;
     
-    return true;
 }
 
 
@@ -73,6 +81,9 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
+    if(isContinued='y'||isContinued=='Y') result =true;
+    else return false;
+    
 
     return result;
 }
@@ -87,6 +98,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
+    cin>>isContinued;
 
     return isContinued;
 }
