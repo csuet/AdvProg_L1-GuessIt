60275e054ac08d39d5a21a1d737e99e333274a16
Quyen <21020787@vnu.edu.vn>
2022-03-11 15:29:33 +0700

    test

--- guessit.cpp
+++ guessit.cpp
@@ -48,7 +48,7 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-    if (numer > randomNumber) answer = "Your number is higher";
+    if (number > randomNumber) answer = "Your number is higher";
     else if (number < randomNumber) answer = "Your number is lower";
     else answer = "Congratulation! You win.";
     return answer;
