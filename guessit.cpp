900ef0403a7b6ed82083a06353361c1e37640ae5
NV.Hoàng <100185897+hoanggcoder@users.noreply.github.com>
2022-03-12 14:19:04 +0700

    Add files via upload

--- guessit.cpp
+++ guessit.cpp
@@ -13,8 +13,8 @@ using namespace std;
         number (int) : random number in range 1-100
 ***/
 int generateRandomNumber() {
-    // TODO: Return the random number in range 1 to 100
-    return 100;
+    
+    return rand()%100+1;
 }
 
 
@@ -26,8 +26,9 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+	int x;
+	cin>>x;
+    return x;
 }
 
 
@@ -46,7 +47,9 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+	if(number==randomNumber) answer="Congratulation! You win.";
+	if(number<randomNumber) answer="Your number is lower.";
+	if(number>randomNumber) answer="Your number is higher.";
     return answer;
 }
 
@@ -59,8 +62,9 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if(answer=="Congratulation! You win.")
+    {return true;}
+    return false;
 }
 
 
@@ -73,7 +77,9 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+	if(isContinued=="1"){
+		result=true;
+	}
     return result;
 }
 
@@ -87,7 +93,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+	cin>>isContinued;
     return isContinued;
 }
 
