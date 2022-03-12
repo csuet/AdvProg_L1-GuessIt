4def3c9665a542e2e3bd90c7ec84421af90daf39
NV.Hoàng <100185897+hoanggcoder@users.noreply.github.com>
2022-03-12 14:33:42 +0700

    Add files via upload

--- guessit.cpp
+++ guessit.cpp
@@ -76,7 +76,9 @@ bool checkSuccess(string answer) {
 ***/
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
-    bool result = false;
+    if(isContinued=='0')
+    {bool result = false;}
+    else result=true;
     return result;
 }
 
