7aaba873334dcab4396448341afca16263a760b4
HitoBoy <100589413+HitoBoy@users.noreply.github.com>
2022-03-13 10:10:12 +0700

    Update guessit.cpp

--- guessit.cpp
+++ guessit.cpp
@@ -82,7 +82,7 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result;
-    if (isContinued == 'Y')
+    if (isContinued == 'Y' || isContinued == 'y')
         result = true;
     else
         result = false;
