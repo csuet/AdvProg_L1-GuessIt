2187799850277eb38a92c076ab227db4f0c0012d
Phung Hoang <hoang9555266@gmail.com>
2022-03-13 22:10:16 +0700

    222

--- guessit.cpp
+++ guessit.cpp
@@ -14,7 +14,9 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return rand() % 100 + 1;
+    srand(time(NULL));
+    int randomNumber = rand() % 100 + 1;
+    return randomNumber;
 }
 
 
@@ -47,7 +49,7 @@ string getAnswer(int number, int randomNumber) {
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
-    string answer;
+   string answer;
     if(number>randomNumber) answer="Your number is higher.";
     else{
         if(number<randomNumber) answer= "Your number is lower.";
@@ -71,7 +73,7 @@ bool checkSuccess(string answer) {
     } else {
         check = false;
     }
-    return true;
+    return check;
 }
 
 
