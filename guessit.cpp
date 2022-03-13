ee6e5e8169afe98f242fd57f24284ba3a0f141a4
GPhong812 <21020128@vnu.edu.vn>
2022-03-13 18:00:41 +0700

    Pham Gia Phong 21020128

--- guessit.cpp
+++ guessit.cpp
@@ -6,69 +6,117 @@
 using namespace std;
 
 
-#include<iostream>
-#include<stdlib.h>
-#include<time.h>
- 
-using namespace std;
-int generateRandomNumber();
-int getPlayerGuess();
-void printAnswer(int guess,int secretNumber,int score);
-int checkscore(int guess,int secretNumber,int &score);
-int Playonegame();
-bool askifcontinue(){int x;
-	cout<<endl<<"Do you want to play a new game???"<<endl<<"Press 1 if you want or Press an another key to exit"<<endl;
-	cin>>x;
-	if(x==1) return true;
-	else return false;
+/***
+    Args:
+        
+    Returns:
+        number (int) : random number in range 1-100
+***/
+int generateRandomNumber() {
+    // TODO: Return the random number in range 1 to 100
+    srand(time(NULL));
+    int randomNumber = rand() % 100 + 1;
+    return randomNumber;
 }
-int run(){
-	bool Playagain;
-	do{
-		Playonegame();
-		Playagain=askifcontinue();
-	}while(Playagain);
-	return 0;
-}
- 
-int Playonegame(){
-	int score =100;
-	cout<<"WELCOME TO GUESS It!"<<endl;
-	cout<<"Your Score is 100 "<<endl;
+
+
+/***
+    Args:
+        
+    Returns:
+        number (int) : the number that player guessed
+***/
+int getPlayerGuess() {
+    // TODO: Ask the player guest and return the player's number
     int guess;
- 
-int secretNumber = generateRandomNumber();
-do{
-    guess=getPlayerGuess();
-    score=checkscore(guess,secretNumber,score);
-    printAnswer(guess, secretNumber,score);
-} 
-while (guess!=secretNumber&&score>0); 
-return 0;
+    cin >> guess;
+    return guess;
 }
-int generateRandomNumber(){srand(time(NULL));
-return rand()%100+1;
+
+
+/***
+    Args:
+        number (int): answer number from player
+        randomNumber (int): the true number
+    Returns:
+        answer (string) : answer of computer after checking result
+***/
+string getAnswer(int number, int randomNumber) {
+    /***
+        TODO: check number with randomNumber and return the result.
+              If number is higher than randomNumber, the answer is "Your number is higher."
+              If number is lower than randomNumber, the answer is "Your number is lower."
+              If number is equal randomNumber, the answer is "Congratulation! You win."
+    ***/
+    string answer;
+    if (number > randomNumber) answer = "Your number is higher.";
+    else
+        if (number < randomNumber) answer = "Your number is lower.";
+        else answer = "Congratulation! You win.";
+    return answer;
 }
-int getPlayerGuess(){
-    int guess;
-cout << endl << "Enter your guess (1..100): ";
-cin >> guess;
-return guess;
+
+
+/***
+    Args:
+        answer (string): answer from computer after compare numbers
+    Returns:
+        result (bool) : player win or not
+***/
+bool checkSuccess(string answer) {
+    // TODO: return the result after checking that player guessed right or wrong
+    if (answer == "Congratulation! You win.") return true;
+    else return false;
 }
-void printAnswer(int guess,int secretNumber,int score){
-if (guess > secretNumber) {
-cout << "Your number is too big." << endl;
-cout << "Your score is "<<score << endl;
- 
-} else if (guess < secretNumber) {
-cout << "Your number is too small." << endl;
-cout << "Your score is "<<score << endl;
-} else { 
-cout << "Congratulation! You win." << endl;
- 
+
+
+/***
+    Args:
+        isContinued (char): player's choice
+    Returns:
+        result (bool) : continue playing or not
+***/
+bool checkContinuePlaying(char isContinued) {
+    // TODO: return result after checking player continue playing or not
+    bool result = false;
+    if (isContinued == 'y' || isContinued == 'Y') result = true;
+    return result;
 }
+
+
+/***
+    Args:
+        
+    Returns:
+        isContinues (char) : player's choice (continue playing or not)
+***/
+char getPlayerOpinion() {
+    // TODO: Ask the player about continue playing and return the player's choice
+    char isContinued;
+    cout << "Do you want to play again? (y/n) ";
+    cin >> isContinued;
+    return isContinued;
 }
-int checkscore(int guess,int secretNumber,int &score){
-	if(guess!=secretNumber) score=score-10;
-	return score;	
+
+
+void playGuessIt() {
+    int randomNumber = generateRandomNumber();
+    int number;
+    string answer;
+    
+    do {
+        number = getPlayerGuess();
+        answer = getAnswer(number, randomNumber);
+        cout << answer << endl;
+    } while (!checkSuccess(answer));
+}
+
+int run() {
+    srand(time(0));
+    char isContinued;
+    do {
+        playGuessIt();
+        isContinued = getPlayerOpinion();
+    } while (checkContinuePlaying(isContinued));
+    return 0;
 }
