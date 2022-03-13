cf6db975968461c294b47b1ed1bbb253f82a1ef2
kimchiiiiii <21020287@vnu.edu.vn>
2022-03-13 10:58:01 +0700

    guess it

--- guessit.cpp
+++ guessit.cpp
@@ -13,8 +13,7 @@ using namespace std;
         number (int) : random number in range 1-100
 ***/
 int generateRandomNumber() {
-    // TODO: Return the random number in range 1 to 100
-    return 100;
+    return rand() % 100 + 1;
 }
 
 
@@ -25,8 +24,10 @@ int generateRandomNumber() {
         number (int) : the number that player guessed
 ***/
 int getPlayerGuess() {
-    // TODO: Ask the player guest and return the player's number
-
+    int guess;
+    cout << "Enter your guess (1...100): ";
+    cin >> guess;
+    return guess;
     return 1;
 }
 
@@ -46,7 +47,15 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+    if (guess == secretNumber) {
+        answer = "Congratulation! You win.";
+    }
+    else if (guess > secretNumber) {
+        answer = "Your number is higher.";
+    }
+    else {
+        answer = "Your number is lower.";
+    }
     return answer;
 }
 
@@ -59,8 +68,12 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if (answer == "Congratulation! You win.") {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 
@@ -73,7 +86,9 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if (isContinued == 'y' || isContinued == 'Y') {
+        result = true;
+    }
     return result;
 }
 
@@ -87,7 +102,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    cout << "Do you want to continue playing? ";
+    cin >> isContinued;
     return isContinued;
 }
 
