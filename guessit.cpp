8289e33b0d799ceb659050e36428bf588ea26394
21020020daovuminhkhanh <21020020@vnu.edu.vn>
2022-03-13 20:54:31 +0700

    21020020 Dao Vu Minh Khanh

--- guessit.cpp
+++ guessit.cpp
@@ -77,8 +77,8 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-    if (isContinued == 'y') result = true;
-    if (isContinued == 'n') result = false;
+    if (isContinued == 'y' || isContinued == 'Y') result = true;
+    if (isContinued == 'n' || isContinued == 'N') result = false;
     return result;
 }
 
