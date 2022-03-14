d730d6a7e15ffff8d11c7dd7f4b5fb8833c0e874
21020276 <100508571+21020276@users.noreply.github.com>
2022-03-14 14:26:49 +0700

    Update guessit.cpp

--- guessit.cpp
+++ guessit.cpp
@@ -13,8 +13,10 @@ using namespace std;
         number (int) : random number in range 1-100
 ***/
 int generateRandomNumber() {
-    // TODO: Return the random number in range 1 to 100
-    return 100;
+	srand(time(0));
+    int RandomNumber = rand() % 100 + 1;
+    
+    return RandomNumber;
 }
 
 
@@ -26,8 +28,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+	int guess;
+ 	cout << endl << "Enter your guess (1..100): ";
+ 	cin >> guess;
+    return guess;
 }
 
 
@@ -46,7 +50,14 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
-
+	if(number > randomNumber){
+		answer="Your number is higher.";
+	}
+	else if (number < randomNumber){
+		answer = "Your number is lower.";
+	}
+	else 
+		answer = "Congratulation! You win.";	
     return answer;
 }
 
@@ -59,8 +70,10 @@ string getAnswer(int number, int randomNumber) {
 ***/
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
-    
-    return true;
+    if	(answer == "Congratulation! You win.")
+    	return true;
+    else 
+		return false;
 }
 
 
@@ -73,7 +86,10 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+	if( isContinued == 'y'|| isContinued=='Y'){
+		result = true;
+	} else if ( isContinued == 'N'|| isContinued=='n')
+		result = false;	
     return result;
 }
 
@@ -87,7 +103,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+	cout << "Do you want to play again ?";
+	cin >> isContinued;
     return isContinued;
 }
 
@@ -112,4 +129,15 @@ int run() {
         isContinued = getPlayerOpinion();
     } while (checkContinuePlaying(isContinued));
     return 0;
+}
+
+
+void printAnswer(int guess, int secretNumber){
+ 	if (guess > secretNumber) {
+ 		cout << "Your number is too big." << endl;
+ 	} else if (guess < secretNumber) {
+ 		cout << "Your number is too small." << endl;
+ 	} else {
+ 		cout << "Congratulation! You win." << endl;
+ 	}
 }
