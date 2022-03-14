6825df3873d40fb313fe2d6096a27f76dc8a23ed
tthanh25 <21020790@vnu.edu.vn>
2022-03-14 12:32:16 +0700

    First commit

--- guessit.cpp
+++ guessit.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include "guessit.h"
 
 using namespace std;
 
@@ -14,7 +13,7 @@ using namespace std;
 ***/
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
-    return 100;
+    return rand() % 100 + 1;   
 }
 
 
@@ -26,8 +25,10 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
-
-    return 1;
+    int guess;
+    cout << endl << "Enter your guess (1..100): ";
+    cin >> guess;
+    return guess;
 }
 
 
@@ -46,6 +47,9 @@ string getAnswer(int number, int randomNumber) {
               If number is equal randomNumber, the answer is "Congratulation! You win."
     ***/
     string answer;
+    if (number < randomNumber) answer = "Your number is lower.";
+    else if (number > randomNumber) answer = "Your number is higher.";
+    else answer =  "Congratulation! You win.";
 
     return answer;
 }
@@ -60,7 +64,8 @@ string getAnswer(int number, int randomNumber) {
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
     
-    return true;
+    if (answer ==  "Congratulation! You win.") return true;
+    else return false;
 }
 
 
@@ -73,7 +78,8 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
-
+    if (isContinued == 'y') result = true;
+    else if (isContinued == 'n') result = false;
     return result;
 }
 
@@ -87,8 +93,10 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
-    return isContinued;
+    cout << "Continue play? (yes/no) :";
+    cin >> isContinued;
+    if (isContinued == 'y' || isContinued == 'n') return isContinued;
+    else return getPlayerOpinion();
 }
 
 
