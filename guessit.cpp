f7a147fddc1c71bb0eefd59e575419fdfd0d565c
Hoàng Nhật Minh <100121365+HoangNhatMinh0302@users.noreply.github.com>
2022-03-15 04:19:34 +0700

    First commit

--- guessit.cpp
+++ guessit.cpp
@@ -4,28 +4,10 @@
 #include "guessit.h"
 
 using namespace std;
-int generateRandomNumber() {
-    // TODO: Return the random number in range 1 to 100
-    return rand() % 100 + 1;
-    srand(time(NULL));
-    int randomNumber = rand() % 100 + 1;
-    return randomNumber;
-}
-
-
-string getAnswer(int number, int randomNumber) {
-              If number is lower than randomNumber, the answer is "Your number is lower."
-              If number is equal randomNumber, the answer is "Congratulation! You win."
-    ***/
-   string answer;
-   string answer;
-    if(number>randomNumber) answer="Your number is higher.";
-    else{
-        if(number<randomNumber) answer= "Your number is lower.";
-bool checkSuccess(string answer) {
-    } else {
-        check = false;
-    }
-    return true;
-    return check;
+ string answer;
+       if(number < randomNumber) answer="Your number is lower.";
+       else(number >randomNumber) answer="Your number is higher.";
+       else if(number >randomNumber) answer="Your number is higher.";
+       else answer="Congratulation! You win.";
+    return answer;
 }
