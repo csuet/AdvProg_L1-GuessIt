7d6317ecd672ee4d27d5d3dea55c524aa5ab94d9
congnghiahieu <21020540@vnu.edu.vn>
2022-03-14 17:19:30 +0700

    21020540 Cong Nghia Hieu

--- guessit.cpp
+++ guessit.cpp
@@ -87,7 +87,7 @@ bool checkSuccess(string answer)
 bool checkContinuePlaying(char isContinued)
 {
     // TODO: return result after checking player continue playing or not
-    if (isContinued == 'c')
+    if (isContinued == 'Y' || isContinued == 'y')
     {
         return true;
     }
