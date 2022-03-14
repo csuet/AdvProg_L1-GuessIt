4a41d84023dde246847eeb794757c44efab2cd49
Nguyen Thi Hong Hanh <21020016@vnu.edu.vn>
2022-03-14 11:00:10 +0700

    try

--- guessit.cpp
+++ guessit.cpp
@@ -77,7 +77,7 @@ bool checkSuccess(string answer) {
 ***/
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
-    bool result = (isContinued == '1');
+    bool result = (isContinued == 'y');
 
     return result;
 }
