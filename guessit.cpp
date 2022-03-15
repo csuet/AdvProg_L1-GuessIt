0067f0c81029a777c1fc77f379757da974246ff1
thanhttruc <100545202+thanhttruc@users.noreply.github.com>
2022-03-15 11:01:50 +0700

    Update guessit.cpp

--- guessit.cpp
+++ guessit.cpp
@@ -33,7 +33,6 @@ string getAnswer(int number, int randomNumber) {
 bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
 
-    return true;
     if (answer == "Congratulation! You win.") return true;
 return false;
 }
