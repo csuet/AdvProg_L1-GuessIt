fe2c50e58ea19f68e4ad6f642abcc97ab0423539
thanhttruc <100545202+thanhttruc@users.noreply.github.com>
2022-03-15 10:04:18 +0700

    Update guessit.cpp

--- guessit.cpp
+++ guessit.cpp
@@ -15,6 +15,7 @@ using namespace std;
 int generateRandomNumber() {
     // TODO: Return the random number in range 1 to 100
     return 100;
+    return (rand() % 100 + 1);
 }
 
 
@@ -26,6 +27,9 @@ int generateRandomNumber() {
 ***/
 int getPlayerGuess() {
     // TODO: Ask the player guest and return the player's number
+    int guess;
+    cin>>guess;
+    return guess;
 
     return 1;
 }
@@ -44,9 +48,15 @@ string getAnswer(int number, int randomNumber) {
               If number is higher than randomNumber, the answer is "Your number is higher."
               If number is lower than randomNumber, the answer is "Your number is lower."
               If number is equal randomNumber, the answer is "Congratulation! You win."
+             
     ***/
     string answer;
-
+     if (number> randomNumber) answer = "Your number is higher." ;
+              } else if (guess < secretNumber) {
+                         answer = "Your number is lower.";
+                    } else {
+                        answer = "Congratulation! You win." ;
+                             }
     return answer;
 }
 
@@ -61,6 +71,8 @@ bool checkSuccess(string answer) {
     // TODO: return the result after checking that player guessed right or wrong
     
     return true;
+    if (answer== "Congratulation! You win." ) return true;
+        else return false;
 }
 
 
@@ -73,6 +85,8 @@ bool checkSuccess(string answer) {
 bool checkContinuePlaying(char isContinued) {
     // TODO: return result after checking player continue playing or not
     bool result = false;
+    if (isContinued == "Y" or isContinued =="y" ) 
+        result=true;
 
     return result;
 }
@@ -87,7 +101,8 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-
+    printf ("Press 'y' for continuing and 'n' for stoping\n");
+    cin >> isContinued;
     return isContinued;
 }
 
